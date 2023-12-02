c60fa6f7b65add3f36b7c9a25df03e0d44ee3411
Nathan Towe <nathanwtowe89@gmail.com>
2023-12-02 13:13:29 -0500

    finished

---
+++ Models/Pokemon.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const PokemonSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    pokemonId: {
+        type: Number,
+        required: true
+    },
+    height: Number,
+    weight: Number,
+    imageSrc: String
+
+})
+
+module.exports = mongoose.model('Pokemon', PokemonSchema)
